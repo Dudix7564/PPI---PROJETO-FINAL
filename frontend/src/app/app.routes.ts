3e82f24e56ab285d925406a0ef782acb44c4d9a4
Maria Eduarda Pimentel de Albuquerque <20221144010017@ifrn.local>
2025-12-04 10:15:18 -0300

    ajustes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './components/login/login';
+import { CadastroUsuario } from './components/cadastro-usuario/cadastro-usuario';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro-usuario', component: CadastroUsuarioComponent },
+  // aqui depois você pode adicionar outras rotas
+  // { path: 'inventario', component: InventarioComponent },
+  // { path: 'cadastro-item', component: CadastroItemComponent },
+  // { path: 'reservas', component: ReservaItemComponent },
+];
