2c5abf8c9be216219f5eb5a39b7f1dc6f97a5ac7
Maria Eduarda <duda891098@gmail.com>
2025-12-05 08:32:49 -0300

    atualizando

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
+import { CadastrarItem } from './components/cadastrar-item/cadastrar-item';
+import { EditarItem } from './components/editar-item/editar-item';
+import { EntradaSaida } from './components/entrada-saida/entrada-saida';
+import { ReservarItem } from './components/reservar-item/reservar-item';
+import { ListaReservas } from './components/lista-reservas/lista-reservas';
+import { PaginaRestrita } from './components/pagina-restrita/pagina-restrita';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+
+  // Área do funcionário
+  { path: 'cadastrar-item', component: CadastrarItem },
+  { path: 'editar-item/:id', component: EditarItem },
+  { path: 'entrada-saida', component: EntradaSaida },
+
+  // Área da comunidade
+  { path: 'reservar-item/:id', component: ReservarItem },
+  { path: 'lista-reservas', component: ListaReservas },
+
+  // Página bloqueada
+  { path: 'pagina-restrita', component: PaginaRestrita }
+];
